85a6fb014679834fd21ff2001a677c45e419deeb
James Reid <jreidmke@gmail.com>
2020-07-10 09:19:03 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let percentOne = (document.getElementById('percentOne'));
+let percentTwo = document.getElementById('percentTwo');
+const btn = document.getElementById("submit");
+const result = document.getElementById('result');
+
+function percentDiff(a, b) {
+    const c = b - a;
+    const apnd = c > 0 ? '% increase' : '% decrease'
+    const diff = a / 100;
+    return Math.abs(c/diff).toFixed(2) + apnd;
+}
+
+btn.addEventListener('click', function(e) {
+    result.innerText = percentDiff(percentOne.value, percentTwo.value);
+    e.preventDefault();
+});
